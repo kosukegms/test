75217b59e2be40c5ef94dc6f86bdaae91456bc21
ryo <ryoatsutagms@gmail.com>
2016-12-01 14:06:52 -0800

    Update

--- test.js
+++ test.js
@@ -4,7 +4,12 @@ var array = [1,2,3,4,5,6,7,8,9];
 
 
 function displayOnlyFive(inputArray){
-
+  inputArray.forEach(function(number){
+    if(number === 6){
+      return console.log(number + " is five");
+    }
+    console.log(number + " is not five");
+  });
 }
 
 displayOnlyFive(array);
