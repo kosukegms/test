281337758c058290c4607606a7c4a4a3005cc3ce
masashiGMS <masashi@gmsnet.com>
2016-12-01 16:55:41 -0800

    no message

--- test2.js
+++ test2.js
@@ -6,10 +6,10 @@ var array = [1,2,3,4,5,6,7,8,9];
 function displayOverFive(inputArray){
 
   inputArray.forEach(function(number){
-    if(number < 5){
-      return console.log(number + " is five");
+    if(number < 3){
+      return console.log(number + " is larger than five");
     }
-    console.log(number + " is not five");
+    
   });
 
 }
