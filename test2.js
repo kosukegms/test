77c4d5056c6b77f0c8032f42de373b79b59f1452
masashiGMS <masashi@gmsnet.com>
2016-12-01 16:53:37 -0800

    no message

---
+++ test2.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var array = [1,2,3,4,5,6,7,8,9];
+
+
+function displayOverFive(inputArray){
+
+  inputArray.forEach(function(number){
+    if(number < 5){
+      return console.log(number + " is five");
+    }
+    console.log(number + " is not five");
+  });
+
+}
+
+displayOnlyFive(array);
