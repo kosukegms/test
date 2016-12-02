2e625dd98abad5d7f01d9f92ea0178f40b5200a7
masashiGMS <masashi@gmsnet.com>
2016-12-01 17:00:12 -0800

    Update: modify if statement

--- test2.js
+++ test2.js
@@ -6,8 +6,8 @@ var array = [1,2,3,4,5,6,7,8,9];
 function displayOverFive(inputArray){
 
   inputArray.forEach(function(number){
-    if(number < 4){
-      return console.log(number + " is larger than four");
+    if(number < 2){
+      return console.log(number + " is larger than two");
     }
 
   });
